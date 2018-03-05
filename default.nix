75b603a67de7e314378566dea2cd7bc60176c869
Vincent Ambo <tazjin@gmail.com>
2018-03-05 23:22:49 +0100

    feat: Add simple Nix derivation for installing script
    Adds a Nix derivation that patches up the script to be
    Nix-compatible (ironically?!)

---
+++ default.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs; stdenv.mkDerivation {
+  name = "nixos-deploy";
+  src = ./.;
+
+  buildPhase = ''
+    # The script expects the Nix file defining the options to be
+    # stored in the same directory, which is not the case in the
+    # derivation output:
+    sed -i "s:^SCRIPT_DIR.*$:SCRIPT_DIR=$out/share:g" nixos-deploy.sh
+
+    # Some external commands aren't correctly recognised by Nix' shell
+    # script patcher:
+    sed -i "s:python:${python}/bin/python:g" nixos-deploy.sh
+    sed -i "s:nix-build:${nix}/bin/nix-build:g" nixos-deploy.sh
+ '';
+
+  installPhase = ''
+    mkdir -p $out/share $out/bin
+    cp nixos-config.nix $out/share/
+    cp nixos-deploy.sh $out/bin/nixos-deploy
+  '';
+}
